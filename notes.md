060abe9399d4dc2a54f53966c363f9fa0bf503d9
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-08-11 11:45:47 -0300

    test: submit lead form and validate toast message

--- notes.md
+++ notes.md
@@ -1,5 +1,37 @@
 # Playwright
 
 ## Commands
+
+### Running tests
+
 To run playwright tests we need to run `npx playwright test`
+
 **Note:** playwright standard is to run tests in headless mode, you can run the assisted execution by adding --headed statement
+
+Some common statements are:
+
+- `--headed` to run the test in assisted mode
+- `debug` to run the test execution step by step in debbuger mode
+- `ui` to open playwright gui page
+
+## Tricks
+
+### Temporary elements
+
+To work with temporary components like toasts you can set a checkpoint or timeout to wait for the toast
+
+```js
+await page.getByText('seus dados conosco').click();
+```
+
+use playwright ui `npx playwright test --ui` to look for it
+
+and then use the following code to get page html content and look for the complete element you need
+
+```js
+// handling toast and temporary elements
+const content = await page.content();
+console.log(content);
+```
+
+**Note:** it might be required when working with temporary elements that are hard to interact with
--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -1,9 +1,24 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('should register a lead in the waiting list', async ({ page }) => {
+test('cadastrar um lead na fila de espera', async ({ page }) => {
   await page.goto('http://localhost:3000');
-  await page
-    .getByRole('button', { name: /Aperte o play/})
-    .click();
+  await page.getByRole('button', { name: /Aperte o play/ }).click();
+
+  // modal checkpoint
+  await expect(page.getByTestId('modal').getByRole('heading')).toHaveText(
+    'Fila de espera'
+  );
+
+  // submit modal
+  await page.getByPlaceholder('Seu nome completo').fill('Customer User');
+  await page.getByPlaceholder('Seu email principal').fill('customer@test.com');
+  await page.getByTestId('modal').getByText('Quero entrar na fila!').click();
+
+  // validate modal
+  const message =
+    'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!';
+  await expect(page.locator('.toast')).toHaveText(message);
+
+  await expect(page.locator('.toast')).toBeHidden({ timeout: 5000 });
 });
