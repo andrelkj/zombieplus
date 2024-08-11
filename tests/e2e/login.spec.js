ff585c704485495ca2d2b4d2ba6a94eaef800821
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-08-11 17:46:44 -0300

    dev: validate user logged in state by looking for the log out button

--- tests/e2e/login.spec.js
+++ tests/e2e/login.spec.js
@@ -1,8 +1,14 @@
 const { test, expect } = require('@playwright/test');
+const { LoginPage } = require('../pages/LoginPage');
+
+let loginPage;
+
+test.beforeEach(({ page }) => {
+  loginPage = new LoginPage(page);
+});
 
 test('deve logar como administrador', async ({ page }) => {
-  await page.goto('http://localhost:3000/admin/login');
-
-  const loginForm = page.locator('.login-form');
-  await expect(loginForm).toBeVisible();
+  await loginPage.visit();
+  await loginPage.submit('admin@zombieplus.com', 'pwd123');
+  await loginPage.isLoggedIn();
 });
