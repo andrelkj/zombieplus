c00dc75c8c6fe717921160d6db3e081009fd20c8
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-08-11 22:37:33 -0300

    test: add login action and validation steps as precondition to the movies test case

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -1,5 +1,22 @@
 const { test } = require('@playwright/test');
 
+const { LoginPage } = require('../pages/LoginPage');
+const { MoviesPage } = require('../pages/MoviesPage');
+const { Toast } = require('../pages/Components');
+
+let loginPage;
+let moviesPage;
+let toast;
+
+test.beforeEach(({ page }) => {
+  loginPage = new LoginPage(page);
+  moviesPage = new MoviesPage(page);
+  toast = new Toast(page);
+});
+
 test('deve poder cadastrar um novo filme', async ({ page }) => {
   // é importante estar logado
+  await loginPage.visit();
+  await loginPage.submit('admin@zombieplus.com', 'pwd123');
+  await moviesPage.isLoggedIn();
 });
