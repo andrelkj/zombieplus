ebce6934d7d24df22847f2c2de37f893f0d6247b
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-08-13 23:43:05 -0300

    test: add movie covers to fixtures and attach images to the movie registration form

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -7,12 +7,7 @@ test('deve poder cadastrar um novo filme', async ({ page }) => {
   const movie = data.create;
   await executeSQL(`DELETE FROM public.movies WHERE title = '${movie.title}';`);
   await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin');
-  await page.movies.create(
-    movie.title,
-    movie.overview,
-    movie.company,
-    movie.release_year
-  );
+  await page.movies.create(movie);
   await page.toast.containText('Cadastro realizado com sucesso!');
 });
 
--- tests/support/actions/Movies.js
+++ tests/support/actions/Movies.js
@@ -13,10 +13,10 @@ export class Movies {
     await this.page.getByRole('button', { name: 'Cadastrar' }).click();
   }
 
-  async create(title, overview, company, release_year) {
+  async create(movie) {
     await this.goForm();
-    await this.page.getByLabel('Titulo do filme').fill(title);
-    await this.page.getByLabel('Sinopse').fill(overview);
+    await this.page.getByLabel('Titulo do filme').fill(movie.title);
+    await this.page.getByLabel('Sinopse').fill(movie.overview);
 
     // select company by id
     await this.page
@@ -25,16 +25,18 @@ export class Movies {
 
     await this.page
       .locator('.react-select__option')
-      .filter({ hasText: company })
+      .filter({ hasText: movie.company })
       .click();
 
     // select release year
     await this.page.locator('#select_year .react-select__indicator').click();
-
     await this.page
       .locator('.react-select__option')
-      .filter({ hasText: release_year })
+      .filter({ hasText: movie.release_year })
       .click();
+
+    // attach poster image
+    await this.page.locator('input[name="cover"]').setInputFiles('tests/support/fixtures' + movie.cover)
 
     await this.submit();
   }
