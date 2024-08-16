3e37ece997e96f7f43cd5462846f69308a186270
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-08-16 00:27:56 -0300

    test: validate duplicated series title and popup message

--- tests/e2e/series.spec.js
+++ tests/e2e/series.spec.js
@@ -19,7 +19,21 @@ test('deve poder remover uma série', async ({ page, request }) => {
   await request.api.postSerie(serie);
 
   await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin');
-  await page.series.go()
+  await page.series.go();
   await page.series.remove(serie.title);
   await page.popup.haveText('Série removida com sucesso.');
+});
+
+test('não deve cadastrar quando o título é duplicado', async ({
+  page,
+  request,
+}) => {
+  const serie = data.duplicate;
+  await request.api.postSerie(serie);
+
+  await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin');
+  await page.series.create(serie);
+  await page.popup.haveText(
+    `O título '${serie.title}' já consta em nosso catálogo. Por favor, verifique se há necessidade de atualizações ou correções para este item.`
+  );
 });
